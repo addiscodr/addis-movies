639f5a98106654338563ee4675db30297cfd0db9
Esayas F. Gebresilasie <addiscodr@gmail.com>
2026-02-28 13:21:44 +0300

    Add auth_gate.dart and modify login_page.dart

---
+++ lib/auth/auth_gate.dart
@@ -0,0 +1,16 @@
+import 'dart:nativewrappers/_internal/vm/lib/internal_patch.dart';
+
+import 'package:flutter/material.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream:  ,
+        builder: ),
+    );
+  }
+}
